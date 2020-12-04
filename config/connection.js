cfd59d2a838ff2344c32b2fd3baf328caf543663
Sai Purciful <saipurciful@gmail.com>
2020-12-04 00:58:02 -0800

    add more info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "burgers_db"
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db",
 });
 
 connection.connect(function(err) {
