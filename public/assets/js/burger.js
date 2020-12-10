d490d142681ef7d5524c62503415f13300ef8ccb
Sai Purciful <saipurciful@gmail.com>
2020-12-10 11:56:39 -0800

    add files

---
+++ public/assets/js/burger.js
@@ -0,0 +1,46 @@
+$(function(){
+    $(".crate-form").on("submit", function(event){
+        event.preventDefault();
+
+        var newBurger ={
+            burger_name: $("#newburger").val().trim(), devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            console.log("Added new burger");
+            location.reload();
+        });
+    });
+    $(".eatburger").on("click", function(event){
+        event.preventDefault();
+        var devouredState = {
+            devoured: 1
+        }
+    });
+
+    $.ajax("/api/burgers/" +id, {
+        type:"PUT",
+        data: devouredState
+    }).then (function(){
+        console.log("Burger devoured");
+        location.reload();
+    });
+});
+
+$(".deleteburger").on("click", function(event){
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    
+    // send the DELETE request.
+    $.ajax({
+        type: "DELETE",
+        url: "/api/burgers/" + id
+    
+}).then(location.reload());
+});
+
+    
