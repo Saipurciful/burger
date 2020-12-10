27f9d438fa77ce838825a682e9cbbeba74419b49
Sai Purciful <saipurciful@gmail.com>
2020-12-10 12:27:41 -0800

    updated Controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,18 +6,17 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    
-    res.render("index", hbsObject);
+router.get("/", function (req,res){
+  res.redirect("/burgers");
+})
+router.get("/burgers", function(req, res) {
+  burger.all(function(data) {
+    res.render("index", {burgers_data: data});
   });
 });
 
 router.post("/burgers", function(req, res) {
-  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+  burger.create(req.body.burger_name, function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId }); 
     res.redirect("/");
@@ -25,17 +24,10 @@ router.post("/burgers", function(req, res) {
 });
 
 router.put("/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
+  burger.update(req.params.id,
     function(result) {
-      res.redirect("/");
+      console.log(result);
+      res.sendStatus(200);
 
     }
   );
