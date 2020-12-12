c018596ccb03d132ac64beca5a86b080bed1d5b8
Sai Purciful <saipurciful@gmail.com>
2020-12-12 01:46:52 -0800

    added more info in.js

--- config/connection.js
+++ config/connection.js
@@ -17,8 +17,12 @@ if (process.env.JAWSDB_URL){
 });
 };
 
-connection.connect();
-  
-  // Export connection for ORM to use
-  module.exports = connection;
-  
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
