93ddeaf94b87b008ed54a6b78f79a8c259d1773f
Sai Purciful <saipurciful@gmail.com>
2020-12-11 23:25:08 -0800

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,12 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-
-const connection = mysql.createConnection({
+const connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
+{
+   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -11,6 +15,7 @@ const connection = mysql.createConnection({
     database: "burgers_db",
    
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
